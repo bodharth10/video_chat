6495d0ed8fef8a2efb639f777d1caee7b304ac2c
bodharth <bodharthlonkar@gmail.com>
2020-04-17 15:44:49 +0400

    action cable settings added

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,6 +48,9 @@ Rails.application.configure do
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
+  config.action_cable.url = 'wss://stormy-river-51957.herokuapp.com//cable'
+  config.action_cable.allowed_request_origins = [ '*' ]
+
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
